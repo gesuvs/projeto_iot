b0b9006fa3e40acc6bb8b4130ba9c5081728454c
GBernardo10 <g_bernardo10@hotmail.com>
2021-05-27 12:19:17 -0300

    add projeto

---
+++ api-envia-dados/API-envia-dados/config/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  // Insira aqui seus dados do banco NA NUVEM AZURE
+  production: {
+    // altere APENAS username, password, database e host.
+    username: 'gesuvs',
+    password: 'futebol9685GOL#',
+    database: 'projeto-bandtec',
+    host: 'projeto-bandtec.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  },
+
+  // Insira aqui seus dados do banco LOCAL - MySQL Workbench
+  dev: {
+    // altere APENAS username, password e database.
+    username: 'SEU_NOME_DE_USUARIO_DO_BANCO_DE_DADOS -- provavelmente é "root"',
+    password: 'SUA_SENHA_DO_BANCO_DE_DADOS',
+    database: 'SEU DATABASE CRIADO EM "CREATE DATASE"',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  },
+};
+ 
